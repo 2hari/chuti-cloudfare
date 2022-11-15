e06d588e15f7185b1105725feb64abd04dd6059b
Hari <two1821125run@protonmail.com>
2022-11-15 08:16:36 +0530

    ready for test deploy
    all depend. and dev. depend., TailwindCSS and SSR check.

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["crxkwzottpngsuxyhrvr.supabase.in", "lh3.googleusercontent.com"],
+  },
+  // experimental: {
+  //   runtime: "experimental-edge",
+  //   reactRoot: true,
+  // },
+};
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Layout from "@/components/Layout";
+
+export async function getServerSideProps() {
+  // Get all homes
+  // const homes = await prisma.home.findMany();
+  // Pass the data to the Home page
+  return {
+    props: {
+      homes: "5",
+      // homes: JSON.parse(JSON.stringify(homes)),
+    },
+  };
+}
+
+export default function Home({ homes = "" }) {
+  return (
+    <Layout>
+      <h1 className="text-xl font-medium text-gray-800">
+        Top-rated places to stay
+      </h1>
+      <p className="text-gray-500">
+        Explore some of the best places in the world
+      </p>
+      <h1>{homes}</h1>
+    </Layout>
+  );
+}
